fe6ca6a5cd768b2a1f68105a73c23573f101db29
fahad <fahadpk1992@gm,ail.com>
2019-10-23 22:19:47 +0530

    first commit

---
+++ app/src/main/java/fhd/test/codecraft/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package fhd.test.codecraft.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import fhd.test.codecraft.R;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
